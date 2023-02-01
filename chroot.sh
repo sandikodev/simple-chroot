46a866b4e4e7bc32e134eaaf2015ffaf94a6418b
sandikodev <androxoss@hotmail.com>
2023-02-02 05:17:07 +0700

    simple chroot done

---
+++ chroot.sh
@@ -0,0 +1,14 @@
+sudo mkdir /mnt/ubuntu
+echo init folder success
+[ -d /sys/firmware/efi ] && echo “EFI boot on HDD” || echo “Legacy boot on HDD”
+echo here wether any information
+sudo mount $1 /mnt/ubuntu
+sudo mount $1 /mnt/ubuntu/boot/efi
+echo mounting root and boot filesystem success
+for i in /dev /dev/pts /proc /sys /run; do
+	echo detected $i, try to mount em
+	sudo mount -B $i /mnt/ubuntu$i
+	echo mounting $i succeded
+done
+echo finally get ready for chroot
+sudo chroot /mnt
